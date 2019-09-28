6c543ddb86a231e72aee81c5daa52ad7f3bb6724
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 12:02:41 -0400

    segundo commit models

---
+++ Rojas/Miguel/Models/Clown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Miguel.Models
+{
+    public enum TypeList { Mesa, Martillo, Manzana }
+
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name="Nombre Completo")]
+        [MinLength(2)]
+        [MaxLength(24)]
+        public string NickName { get; set; }
+        [Required]
+        public TypeList Tricks { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Display(Name ="Cumpleaños")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+
+    }
+}
