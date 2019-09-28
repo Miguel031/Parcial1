7d316e79af3db09987252cfeb8bed27016c7de09
visual_community@outlook.com <visual_community@outlook.com>
2019-09-28 12:18:19 -0400

    Cuarto commit controllers

--- Rojas/Miguel/Models/DataContext.cs
+++ Rojas/Miguel/Models/DataContext.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Miguel.Models
 {
-    public class DataContext:DbContex
+    public class DataContext:DbContext
     {
         public DataContext():base("DefaultConection")
         {
 
         }
+
+        public System.Data.Entity.DbSet<Miguel.Models.Clown> Clowns { get; set; }
     }
 }
